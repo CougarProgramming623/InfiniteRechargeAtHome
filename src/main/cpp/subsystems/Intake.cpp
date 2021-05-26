0156568d86c812cd1bb41d0f40ff1955555f29b2
Carson Harter <carsonharter@yahoo.com>
2021-05-26 17:25:09 -0400

    Chris Wanted Code -- Dysfunctional

--- src/main/cpp/subsystems/Intake.cpp
+++ src/main/cpp/subsystems/Intake.cpp
@@ -1,82 +1,50 @@
 #include "subsystems/Intake.h"
-
 #include "ohs/RobotID.h"
-
-#include <frc2/command/InstantCommand.h>
-#include <frc2/command/FunctionalCommand.h>
-#include<frc/DigitalInput.h>
+#include "Robot.h"
 
 using namespace ohs623;
 
-namespace ohs2020{
+namespace ohs2020 {
 
 Intake::Intake() :
 
-m_IntakeDown([&]		{ return Robot::Get().GetOI().GetButtonBoard().GetRawButton(10);}),
-m_IntakeStowed([&]   	{ return Robot::Get().GetOI().GetButtonBoard().GetRawButton(9);}),
-m_IntakeUp([&]			{ return !Robot::Get().GetOI().GetButtonBoard().GetRawButton(10) && !Robot::Get().GetOI().GetButtonBoard().GetRawButton(9);}),
+	m_IntakeMover(RobotID::GetID(INTAKE_MOVER)),
+	m_BallMover(RobotID::GetID(BALL_MOVER)),
 
-m_IntakeOn([&] 			{ return Robot::Get().GetOI().GetButtonBoard().GetRawButton(11);}),
-m_IntakeOff([&] 		{ return Robot::Get().GetOI().GetDriverJoystick().GetRawButton(2);}),
-m_IntakeReverse([&] 	{ return Robot::Get().GetOI().GetButtonBoard().GetRawButton(6);}),
+	m_IntakeDown([&] 	{return Robot::Get().GetOI().GetButtonBoard().GetRawButton(10);	}),
+	m_IntakeUp([&] 		{return Robot::Get().GetOI().GetButtonBoard().GetRawButton(9);	}),
 
-m_IntakePositioner	(RobotID::GetID(INTAKE_POSITIONER)),
-m_MainIntakeMotor	(RobotID::GetID(INTAKE_SPIN)),
+	m_IntakeIn([&] 		{return Robot::Get().GetOI().GetButtonBoard().GetRawButton(0);	}),
+	m_IntakeOut([&] 	{return Robot::Get().GetOI().GetButtonBoard().GetRawButton(0);	})
+{
 
-m_MiddleLimit(0){
 
-m_IntakePositioner.SetNeutralMode(Brake);
 
-}
-
-// int POS_FORWARD = .2;
-// int POS_BACKWARDS = .2;
-
-// int MAIN_FORWARD = 1;
-// int MAIN_BACKWARDS = -1;
+}//constructor
 
 void Intake::Init() {
-	// m_IntakePositioner.Set(ControlMode::PercentOutput, POS_FORWARD);
-    SetToggleIntakeButtons();
-	SetPositionButton();
-	m_IntakePositioner.SetInverted(true);
-}
+	MoveIntake();
+}//Init
 
-void Intake::SetPositionButton() {
+void Intake::MoveIntake() {
 	m_IntakeDown.WhileHeld(frc2::RunCommand([&] {
-		 m_IntakePositioner.Set(ControlMode::PercentOutput, -1);
-		 DebugOutF("DOWN");
-	},{}));
-	m_IntakeStowed.WhileHeld(frc2::RunCommand([&] {
-		m_IntakePositioner.Set(ControlMode::PercentOutput, 1);
-		DebugOutF("UP");
-	},{}));
-
-
-	m_IntakeUp.WhenHeld(frc2::FunctionalCommand( [this] { //on init
-		DebugOutF("MIDDLE");
-		lastSwitch = m_IntakePositioner.IsFwdLimitSwitchClosed();
-	}, [this] {
-		if(lastSwitch){
-			m_IntakePositioner.Set(ControlMode::PercentOutput, -1);
-		} else {
-			m_IntakePositioner.Set(ControlMode::PercentOutput, 1);
-		}
-	}, [this] (bool f) { //on end
-        m_IntakePositioner.Set(ControlMode::PercentOutput, 0);
-	}, [this] { //is finished
-		DebugOutF(std::to_string(!m_MiddleLimit.Get()));
-		return !m_MiddleLimit.Get();
+		m_IntakeMover.Set(ControlMode::PercentOutput, 0.1);
+		DebugOutF("Intake Moving Down");
 	}, {}));
-}
+	m_IntakeDown.WhenReleased(frc2::RunCommand([&] {
+		m_IntakeMover.Set(ControlMode::PercentOutput, 0);
+		DebugOutF("Down Stopped");
+	}, {}));
 
-void Intake::SetToggleIntakeButtons() {
+	m_IntakeUp.WhileHeld(frc2::RunCommand([&] {
+		m_IntakeMover.Set(ControlMode::PercentOutput, -0.1);
+		DebugOutF("Intake Moving Up");
+	}, {}));
+	m_IntakeUp.WhenReleased(frc2::RunCommand([&] {
+		m_IntakeMover.Set(ControlMode::PercentOutput, 0);
+		DebugOutF("Up Stopped");
+	}, {}));
 
-	m_IntakeOn.WhileHeld(frc2::RunCommand([&] 		{ m_MainIntakeMotor.Set(ControlMode::PercentOutput, 1);	}, {}));
-	m_IntakeOn.WhenReleased(frc2::InstantCommand([&] 		{ m_MainIntakeMotor.Set(ControlMode::PercentOutput, 0);	}, {}));
-	
-	// m_IntakeOff.WhenPressed(frc2::InstantCommand([&] 		{ m_MainIntakeMotor.Set(ControlMode::PercentOutput, 0);				}, {}));
-	// m_IntakeReverse.WhenPressed(frc2::InstantCommand([&] 	{ m_MainIntakeMotor.Set(ControlMode::PercentOutput, .5);}, {}));
+}//MoveIntake
 
-}
 }//namespace
--- src/main/include/subsystems/Intake.h
+++ src/main/include/subsystems/Intake.h
@@ -2,37 +2,27 @@
 
 #include <ctre/Phoenix.h>
 #include <frc2/command/button/Button.h>
-#include <frc/DigitalInput.h>
 
 #include "Robot.h"
 
-namespace ohs2020{
 
-class Intake {
+namespace ohs2020 {
 
-public:
-	Intake();
+	class Intake {
+		public:
+		Intake();
 
+		void Init();
+		void MoveIntake();
 
-	void Init();
-	void SetPositionButton();
-	void SetToggleIntakeButtons();
+		private:
+		frc2::Button m_IntakeDown;
+		frc2::Button m_IntakeUp;
 
-private:
+		frc2::Button m_IntakeIn;
+		frc2::Button m_IntakeOut;
 
-frc2::Button m_IntakeDown;
-frc2::Button m_IntakeStowed;
-frc2::Button m_IntakeUp;
-
-frc2::Button m_IntakeOn;
-frc2::Button m_IntakeOff;
-frc2::Button m_IntakeReverse;
-
-WPI_TalonSRX m_IntakePositioner;
-WPI_TalonSRX m_MainIntakeMotor;
-
-frc::DigitalInput m_MiddleLimit;
-bool lastSwitch = false;
-
-};
+		WPI_TalonSRX m_IntakeMover;
+		WPI_TalonSRX m_BallMover;
+	};
 }//namespace
