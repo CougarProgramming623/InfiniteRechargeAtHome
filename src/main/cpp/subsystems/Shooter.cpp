cd1ed1287d97ae9a40a2ffad2370011e4c9d74d5
Veljko <veljko.cvetkovich@gmail.com>
2021-09-23 21:13:53 -0400

    More buttons added

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -4,6 +4,8 @@
 
 namespace ohs2020{
 
+    const double DefaultShooterPower = 1;
+
     Shooter::Shooter() : 
 
     m_FlyWheel(RobotID::GetID(FLYWHEEL)),
@@ -13,12 +15,27 @@ namespace ohs2020{
     m_Launcher( [&]         { return Robot::Get().GetOI().GetButtonBoard().GetRawButton(2); }),
     m_FlyWheelToggle( [&]   { return Robot::Get().GetOI().GetButtonBoard().GetRawButton(4); }),
     m_ReverseFeeder( [&]    { return Robot::Get().GetIO().GetButtonBoard().GetRawButton(14); })
-
+    {}
 
     void Shooter::Init(){
         
         SetupShooterButtons();
-        m_Feeder.setSafetyEnabled(false);
+        m_Feeder.setSafetyEnabled(true);
         DebugOutF("Shooter Init");
+    }
+
+    inline bool GetFlyWheelToggleState() {
+        return Robot::Get().GetIO().GetButtonBoard().GetRawButton(4);
+    }
+
+    void Shooter::SetupShooterButtons(){
+
+        m_FlyWheelToggle.WhenPressed(frc2::FunctionalCommand([this]{}, [this] { //on execute
+
+            m_IsFlywheelOn = true;
+            m_FlywheelWU = (int)((double)m_Flywheel.GetSelectedSensorVelocity() / 2048 * 600 );
+            frc::SmartDashboard::PutNumber("Flywheel Speed", m_FlywheelWU);
+
+        }))
     }
 }
